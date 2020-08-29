e43fb4fcc3c74d790e41a6058642129a87e7ed5a
Nadika Poudel <nadikapoudel16@gmail.com>
2020-08-29 09:25:24 +0545

    Reactivity

---
+++ src/views/Product.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="hbody" >
+    <div v-bind:key="product.id" v-for="product in products">
+        <Productitem v-bind:product="product"  v-on:add-to-cart="$emit('add-to-cart',product.id)"/>
+        
+    </div>
+    <p class="cart">Cart:{{cart}}</p>
+    
+</div>
+</template>
+
+<script>
+import Productitem from './Productitem';
+
+export default {
+    name: "Product",
+    props: ["products","cart"],
+    components:{
+        Productitem
+    }
+    
+}
+</script>
---
+++ src/views/Productitem.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="product-item">
+        <p class="product-image"><img v-bind:src="product.image" /></p>
+
+        <h3>{{product.name}}</h3>
+        <div class="product-info">
+        
+        <p v-if="product.inStock">InStock</p>
+        <p v-else>Out Of Stock</p>
+        </div>
+
+        <button  :disabled="!product.inStock"  v-bind:class="{'activebutton': product.inStock}" v-on:click="$emit('add-to-cart',product.id)">Add to Cart</button>
+        
+    </div>
+</template>
+<script>
+export default {
+    name:"ProductItem",
+    props:["product"]
+    
+    }
+
+</script>
